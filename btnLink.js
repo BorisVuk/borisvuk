6bb4a1ee7de32f31a2cb1c850882dc6d1ed5f45d
Alpha <alpha@localhost.localdomain>
2017-12-27 19:19:29 +0100

    AM TOTALY NOT UP TO aesthetics

---
+++ btnLink.js
@@ -0,0 +1,9 @@
+function home(){
+document.location.href="index.html";
+}
+function about(){
+document.location.href="aboutm3.html";
+}
+function contact(){
+document.location.href="contact.html";
+}
